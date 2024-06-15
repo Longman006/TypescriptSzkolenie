44dd71c545d5b7c70c8d80df608b4ba2a9b49aa3
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-15 11:27:37 +0200

    Exercise: JSDoc - fix personName func

--- trener/1_migracja/src/lib.js
+++ trener/1_migracja/src/lib.js
@@ -1,9 +1,26 @@
 
+/**
+ * @typedef Person
+ * @extends Object
+ * @property {string} name
+ */
 
-export function substract(a,b) {
+/**
+ * Odejmuje liczby
+ * 
+ * @param {number} a Odjemnik
+ * @param {number} b Odjemna
+ * @returns {number} Wynik
+ */
+export function substract(a, b) {
     return a - b
 }
 
-export function divide(a,b) {
+export function divide(a, b) {
     return a / b
+}
+
+
+function showPersonName(person) {
+    return person.namee
 }
