d140afee9ccf54febc7a8910317c87b88ff11e9f
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-15 15:54:43 +0200

    Exercise: Interpolate text - React

--- src/app/playlists/components/PlaylistDetails.tsx
+++ src/app/playlists/components/PlaylistDetails.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const PlaylistDetails = (props: Props) => {
-  return (
-    <div>PlaylistDetails</div>
-  )
-}
+  const playlist = {
+    id: "123",
+    name: "Playlist 123",
+    public: false,
+    description: "Best playlist",
+  };
 
-export default PlaylistDetails
+  return <div>
+  {/* .grid.gap-5>.grid.gap-2*3>div.font-semibold{Name}+div{Playlist} */}
+
+  <div className="grid gap-5 mb-5">
+
+    {/* {'ala ma ' + 'kota ' + playlist.name} */}
+
+    <div className="grid">
+      <div className="font-semibold">Name</div>
+      <div>Playlist abc</div>
+    </div>
+
+    <div className="grid">
+      <div className="font-semibold">Public</div>
+      <div>Yes</div>
+    </div>
+
+    <div className="grid">
+      <div className="font-semibold">Description</div>
+      <div>Awesoem Playlist</div>
+    </div>
+
+  </div>
+
+  </div>;
+};
+
+export default PlaylistDetails;
