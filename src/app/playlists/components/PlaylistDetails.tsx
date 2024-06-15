63e4b27b8b7119aba47b41fa99ad7385a19f4ffd
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-15 15:47:31 +0200

    Playlists page - outline, grid, components

---
+++ src/app/playlists/components/PlaylistDetails.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const PlaylistDetails = (props: Props) => {
+  return (
+    <div>PlaylistDetails</div>
+  )
+}
+
+export default PlaylistDetails
---
+++ src/app/playlists/page.tsx
@@ -0,0 +1,32 @@
+// tsrafce
+
+import React from 'react'
+import PlaylistList from './components/PlaylistList'
+import PlaylistEditor from './components/PlaylistEditor'
+import PlaylistDetails from './components/PlaylistDetails'
+
+type Props = {}
+
+const PlaylistsPage = (props: Props) => {
+  return (
+    <div>
+        PlaylistsPage
+
+        {/* .grid.grid-cols-2.gap-4 */}
+        
+        <div className="grid grid-cols-2 gap-4">
+            <div>
+                <PlaylistList/>
+            </div>
+            <div>
+                <PlaylistDetails/>
+                <PlaylistEditor/>
+            </div>
+        </div>
+        
+
+    </div>
+  )
+}
+
+export default PlaylistsPage
