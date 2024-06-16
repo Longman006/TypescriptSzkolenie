431e69f56792d0f33b65f7e6f3a9a8580b7b8724
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-16 09:03:44 +0200

    Object vs object vs {}

--- trener/examples/Object vs object.ts
+++ trener/examples/Object vs object.ts
@@ -1,11 +1,56 @@
 
 
-function takeOnlyObjects(obj: ???){
-
+function takeOnlyObjects1(obj: object) {
+    // obj.????
 }
 
-takeOnlyObjects('')
-takeOnlyObjects(123)
-takeOnlyObjects({})
-takeOnlyObjects([])
-takeOnlyObjects({x:123})
+// takeOnlyObjects1(null)
+// takeOnlyObjects1(undefined)
+// takeOnlyObjects1('')
+// takeOnlyObjects1(123)
+takeOnlyObjects1({})
+takeOnlyObjects1([])
+takeOnlyObjects1({ x: 123 })
+
+
+
+function takeOnlyObjects2(obj: Object) {
+    obj.isPrototypeOf
+    obj.propertyIsEnumerable
+    // obj....
+}
+
+// takeOnlyObjects2(null)
+// takeOnlyObjects2(undefined)
+takeOnlyObjects2('')
+takeOnlyObjects2(123)
+takeOnlyObjects2({})
+takeOnlyObjects2([])
+takeOnlyObjects2({ x: 123 })
+
+
+
+function takeOnlyObjects3(obj: {}) {
+    // obj.????
+    obj.isPrototypeOf({})
+    obj.propertyIsEnumerable('')
+    // obj....
+}
+
+// takeOnlyObjects3(null)
+// takeOnlyObjects3(undefined)
+takeOnlyObjects3('')
+takeOnlyObjects3(123)
+takeOnlyObjects3({})
+takeOnlyObjects3([])
+takeOnlyObjects3({ x: 123 })
+
+
+
+// --------
+
+typeof [] == 'object'; // true
+
+([]) instanceof Object; // Cat
+([]) instanceof Array;  // Tiger
+// Array extends Object
