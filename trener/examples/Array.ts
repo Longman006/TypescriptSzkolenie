6dcf0ba7fa12137faea758a6bda130ee443e3a04
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-16 13:51:11 +0200

    Exercise: Type Nested Array - ticktactoe

---
+++ trener/examples/Array.ts
@@ -0,0 +1,37 @@
+export { }
+
+// Infered types
+//             kola, kwadraty, figury   -> ( null | {} )[]
+// const array = [123, '234', true, [], {}, null]
+
+//  (string | number | boolean | never[] | null)[]
+let array = [123, '234', true, [], null]
+/*  */array = ['234', true, 123, null]
+/*  */array = array.reverse()
+
+
+// typeof array == 'object' // true // JS
+type ArryT = typeof array // TS;
+
+
+// Infered from control flow
+array[0] = 'test'
+const elem = array[0] // string
+
+
+// Typing Arrays
+const numbers1: number[] = [1, 2, 23]
+
+// Generic type 'Array<T>' requires 1 type argument(s).ts(2314)
+const numbers: Array<number> = [123]
+const strings: Array<string> = ['123']
+const booleans: Array<boolean> = [true]
+const modes: Array<'on' | 'off'> = ['off', 'on']
+
+
+// Type Nested Array 
+const ticktactoe = [
+    ["X", "O", "X"],
+    ["O", " ", " "],
+    ["X", "O", "X"],
+];
