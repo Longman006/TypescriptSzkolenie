fc81947b830dea89aa0a37f15dedaf04aa396115
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-16 10:40:26 +0200

    Enum, named enum

--- src/app/playlists/page.tsx
+++ src/app/playlists/page.tsx
@@ -6,17 +6,31 @@ import PlaylistList from "./components/PlaylistList";
 import PlaylistEditor from "./components/PlaylistEditor";
 import PlaylistDetails from "./components/PlaylistDetails";
 
+enum Modes {
+  editor = "editor",
+  details = "details",
+  creator = "creator",
+}
+
 type Props = {};
 
 const PlaylistsPage = (props: Props) => {
-  const [mode, setMode] = useState("details");
+  const [mode, setMode] = useState(Modes.details);
 
-  const showDetails = () => {};
-  const showEditor = () => {};
+  // const [mode, setMode] = useState("details");
+
+  const showDetails = () => {
+    setMode(Modes.details);
+  };
+  const showEditor = () => {
+    setMode(Modes.editor);
+  };
 
   return (
     <div>
       <h1 className="text-4xl leading-loose">Playlists</h1>
+      
+      {mode} {Modes[mode]}
 
       <div className="grid grid-cols-2 gap-7">
         <div>
@@ -25,13 +39,23 @@ const PlaylistsPage = (props: Props) => {
         <div>
           {/* {mode === "details" ? <PlaylistDetails /> : <PlaylistEditor />} */}
 
-          {mode === "details" && <PlaylistDetails />}
+          {mode === Modes.details && <PlaylistDetails />}
 
-          {mode === "details" || <PlaylistEditor />}
+          {mode === Modes.details || <PlaylistEditor />}
 
           <div className="flex justify-between">
-            <button className="bg-red-500 text-white px-5 py-2">Cancel</button>
-            <button className="bg-purple-500 text-white px-5 py-2">Edit</button>
+            <button
+              className="bg-red-500 text-white px-5 py-2"
+              onClick={showDetails}
+            >
+              Cancel
+            </button>
+            <button
+              className="bg-purple-500 text-white px-5 py-2"
+              onClick={(event) => showEditor()}
+            >
+              Edit
+            </button>
           </div>
         </div>
       </div>
