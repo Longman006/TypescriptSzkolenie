1734c550ed3198e02e6959e53a511af5c988b8f4
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-22 14:24:37 +0200

    Covariance // ContraVariance

---
+++ trener/examples/Covariant contravariant.ts
@@ -0,0 +1,39 @@
+
+// Structural (TS) vs Nominal (JAVA) typing
+
+type Vector = { x: number, y: number, length: number }
+type Point = { x: number, y: number }
+
+let v: Vector = { x: 123, y: 234, length: 234 }
+let p: Point = { x: 123, y: 234 }
+
+// Covariance
+// v = p; // Error: Property 'length' is missing in type 'Point' but required in type 'Vector'.ts(2741)
+p = v;
+
+// p.length // Error: Property 'length' does not exist on type 'Point'.ts(2339)  - Infomation hiding
+(p as Vector).length // JS -> OK!
+
+
+// Covariance
+function drawV(v: Vector) { }
+function drawP(p: Point) { }
+
+drawV(v)
+// drawV(p) // missing length
+drawP(v)
+drawP(p)
+
+
+// ContraVariance
+let callbackP: (p: Point) => void
+let callbackV: (v: Vector, color: string) => void
+
+
+// callbackP = (v:Vector) => {} // Error: missing length
+callbackV = (p: Point) => { }
+callbackV = (p: Vector) => { }
+callbackV = (event) => { }
+
+callbackV = () => { }
+callbackP = () => { } 
