044587f1a8f02fe78a9c7d69d673c2206cf14d0e
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-22 10:11:27 +0200

    Dynamic Key + Indexed Type

--- src/app/core/types/Playlist.tsx
+++ src/app/core/types/Playlist.tsx
@@ -10,3 +10,20 @@ interface Track {}
 
 // Type lookup
 type PlaylistId = Playlist["id"];
+
+// Dynamic Key
+const klucz = 123 + 321;
+
+const obj = {
+  [klucz]: 123,
+} as IndexedType;
+
+type IndexedType = {
+  [_: number]: number;
+};
+
+obj[444] === 123;
+obj[666] = 123;
+obj[321] = 123;
+// obj[...] = 123;
+
