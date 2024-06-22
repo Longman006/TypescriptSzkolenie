b0ec6fa84cac42310fdf7214a77341f1b2125dcc
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-22 09:29:02 +0200

    typeof, keyof

--- trener/examples/key exercises.ts
+++ trener/examples/key exercises.ts
@@ -2,12 +2,25 @@
 export { }
 
 
-function getProperty(obj: type, key: typ) {
+// function getProperty(obj: object, key: keyof {} ) {
+// function getProperty(obj: object, key: keyof typeof obj) {
+// function getProperty<T extends object>(obj: T, key: keyof typeof obj) {
+// function getProperty<T extends object>(obj: T, key: keyof T) {
+
+function getProperty<T extends object, K extends keyof T>(obj: T, key: K) {
     return obj[key]
 }
 
+// OK
+getProperty({ a: 1, b: 2 }, 'a')
 
-function pick(obj: type, keys: type) {
+// @ts-expect-error
+getProperty({ x: 'y', z: 'q' }, 'a')
+
+
+
+
+function pick(obj: object, keys: type) {
     return keys.reduce((acc, key) => {
         acc[key] = obj[key]
         return acc
