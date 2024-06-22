5f1158fdd1148b3a7ffd112253ea27d52786599e
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-22 11:00:13 +0200

    Exercise: Conditional Types - only Strings

--- trener/examples/ConditionalTypes.ts
+++ trener/examples/ConditionalTypes.ts
@@ -15,4 +15,22 @@ function getMessage(msg: Message): string {
 }
 
 // @ts-expect-error
-getMessage(123) 
+getMessage(123)
+
+type MessageOf<T> = T extends { message: string } ? T['message'] : never
+
+type result1 = MessageOf<Message>
+type result2 = MessageOf<string> | number
+type result3 = MessageOf<{ message: 'test' }>
+
+// type Flatten<T> = T extends Array<unknown>? T[0] : T
+type Flatten<T> = T extends Array<unknown> ? T[number] : T
+
+type res1 = Flatten<string[][]>
+type res2 = Flatten<string>
+
+
+type keysunion = 'id' | 'name' | never //  "id" | "name"
+
+
+type OnlyStrings<T> = ???
