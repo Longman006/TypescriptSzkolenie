a90fb8c18dc46701cf79f7d8615d2986f63c2be7
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-22 09:42:05 +0200

    keyof typeof + generic extends

--- trener/examples/key exercises.ts
+++ trener/examples/key exercises.ts
@@ -17,6 +17,42 @@ getProperty({ a: 1, b: 2 }, 'a')
 // @ts-expect-error
 getProperty({ x: 'y', z: 'q' }, 'a')
 
+// -----------------------------------
+
+const roles/* : AnyRole */ = {
+    'admin': 'ADMIN_ROLE',
+    'user': 'USER_ROLE',
+    'moderator': 'MODERATOR_ROLE',
+}
+
+type Roles = keyof typeof roles // 'admin' | 'user' | 'moderator'
+
+
+// function setUserRole(user: any, role: 'admin' | 'user' | 'moderator') {
+function setUserRole(role: Roles, user: UserTypes) {
+    if (user.role === role) {
+        return user
+    }
+}
+
+type UserTypes = User | Admin | Moderator
+interface User { id: string, role: Roles }
+interface Admin extends User { role: 'admin' }
+interface Moderator extends User { role: 'moderator' }
+
+
+// function checkUserRole(user: UserTypes, role: ??) { }
+function checkUserRole<T extends UserTypes>(user: T, role: T['role']) { }
+
+checkUserRole({ id: '123', role: 'admin' }, 'admin')
+
+
+
+
+
+
+
+
 
 
 
